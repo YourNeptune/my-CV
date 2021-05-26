dc5606326e820d5f00e18180afaa61a32b099929
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-26 17:50:12 -0400

    add email and github links

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,10 @@ const About = () => {
           <h3>Hello! I'm Lixuan Luo, I love coding and baking.</h3>
           <div>
             <p>
-              I am passionate about
-              self-development and self-improvement in my personal life. I'm
-              always looking to challenge myself and learn new things. After
-              graduating with a bachelor's in computer system engineering, I
-              moved to Toronto to start a new career.
+              I am passionate about self-development and self-improvement in my
+              personal life. I'm always looking to challenge myself and learn
+              new things. After graduating with a bachelor's in computer system
+              engineering, I moved to Toronto to start a new career.
             </p>
             <p>
               I took basic and intermediate certificate courses in Le Cordon
@@ -29,9 +28,25 @@ const About = () => {
               that I built this website on my own in React.js.
             </p>
           </div>
-          <div className='button'>
-            <Button className="resume_button">Resume</Button>
+          <div className="About__info">
+            <p>
+              <span>Email: </span>
+              lixuanluo@hotmail.com
+            </p>
+            <p>
+              <span>Github: </span>
+              <a
+                href="https://github.com/YourNeptune"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://github.com/YourNeptune
+              </a>
+            </p>
           </div>
+          {/* <div className='button'>
+            <Button className="resume_button">Resume</Button>
+          </div> */}
         </div>
       </div>
     </div>
