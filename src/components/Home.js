dc0559139227dd45389a5f9fc4436132ba6012ef
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-26 17:49:44 -0400

    add typed.js animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,34 @@
-import { Button } from "@material-ui/core";
+// import { Button } from "@material-ui/core";
 import "../css/Home.css";
-
+import Typed from "typed.js";
+import { useRef, useEffect } from "react";
 
 const Home = () => {
+  const typeTarget = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typeTarget.current, {
+      strings: [`coder`, `Baker`, `front-end developer`],
+      startDelay: 500,
+      typeSpeed: 40,
+      backSpeed: 20,
+      loop: true,
+      backDelay: 2000,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="Home">
       <div className="Home__title">
+        <h2>I' am</h2>
         <h1 className="Home__name">
           Lixuan <br /> Luo
         </h1>
-        <Button className="resume_button" >
-          Resume
-        </Button>
+        <span className='typedStrings' ref={typeTarget} />
+        {/* <Button className="resume_button">Resume</Button> */}
       </div>
     </div>
   );
